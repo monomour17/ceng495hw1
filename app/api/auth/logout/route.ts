d2da7d03ea88523f0b8f4555a0914d4b7713b92d
Aliriza <alirizaurhan@gmail.com>
2026-04-05 17:06:04 +0300

    logout fix

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function GET() {
+export async function GET(request: Request) {
     const cookieStore = await cookies();
     cookieStore.delete("elob2bauth");
-    return NextResponse.redirect(new URL("/", process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"));
+    return NextResponse.redirect(new URL("/", request.url));
 }
